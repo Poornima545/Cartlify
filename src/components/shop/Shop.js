548fc21d32478b364837a78c4ece929896237569
Poornima545 <poornimac714@gmail.com>
2024-10-28 20:53:59 +0530

    updating in shop file

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Products } from '../Product';
 
 export default function Shop() {
+    const renderStars = (rating) => {
+        const filledStars = '★'.repeat(rating);
+        const emptyStars = '☆'.repeat(5 - rating);
+        return filledStars + emptyStars;
+    };
+
     return (
         <div className='shop'>
             <div className='products'>
@@ -9,8 +15,9 @@ export default function Shop() {
                     Products.map((product) => (
                         <div key={product.id} className='product'>
                             <img src={product.img} alt={product.ProductName} />
-                            <h3>{product.ProductName}</h3>
-                            <p>${product.Price.toFixed(2)}</p>
+                            <h4>{product.ProductName}</h4>
+                            <p>Price: ${product.Price.toFixed(2)}</p>
+                            <p>Rating: <span>{renderStars(product.Rating)}</span></p>
                         </div>
                     ))
                 }
